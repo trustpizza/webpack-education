87d47e3e754cea247c0e2b86a3f8036d17f1a515
trustpizza <maxelolsson@gmail.com>
2023-01-25 14:20:08 -0600

    Re-add lodash

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
-import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.textContent = myName('Cody');
+    element.classList.add('hello');
     return element;
   }
   
